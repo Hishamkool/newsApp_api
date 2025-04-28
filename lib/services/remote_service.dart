61a3024b2415bb2f65aa6306a37cd6f98cc5ddf6
Hisham <linkedin.hisham@gmail.com>
2025-04-28 17:55:49 +0530

    updated the news api link, updated the errors but changing gradle version and agp version an kotlin version to compatable versions.

--- lib/services/remote_service.dart
+++ lib/services/remote_service.dart
@@ -3,12 +3,12 @@ import 'package:news_app_api/models/news_model.dart';
 
 class RemoteNewsService {
   static var client = http.Client();
-  static Future <List<NewsArticle> ? > fetchNewsArticle()async{
-    var response = await client.get(Uri.parse("https://newsapi.org/v2/everything?q=tesla&from=2024-04-05&sortBy=publishedAt&apiKey=b951c0a587d7458d924dbbe3a4a90de4"));
-    if (response.statusCode ==200){
+  static Future<List<NewsArticle>?> fetchNewsArticle() async {
+    var response = await client.get(Uri.parse(
+        "https://newsapi.org/v2/everything?q=tesla&from=2025-04-20&sortBy=publishedAt&apiKey=b951c0a587d7458d924dbbe3a4a90de4"));
+    if (response.statusCode == 200) {
       return newsArticleFromJson(response.body);
-
-    }else{
+    } else {
       return null;
     }
   }
